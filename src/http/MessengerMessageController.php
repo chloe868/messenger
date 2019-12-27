5f99c3eb6c2173e61e1e09ee47f2d65571f1eaa1
kennCK <kennettecanales@gmail.com>
2019-12-27 14:52:36 +0800

    update

--- src/http/MessengerMessageController.php
+++ src/http/MessengerMessageController.php
@@ -33,7 +33,7 @@ class MessengerMessageController extends APIController
         $data['account'] = $this->retrieveAccountDetails($data['account_id']);
         $data['created_at_human'] =  Carbon::now()->copy()->tz('Asia/Manila')->format('F j, Y');
         MessengerGroup::where('id', '=', $data['messenger_group_id'])->update(array('updated_at' => Carbon::now()));
-        Notifications::dispatch('message', $data->toArray());
+        Notifications::dispatch('message', $data);
         // app('App\Http\Controllers\EmailController')->newMessage($data['account_id']);
       }else{
         $error = "Something went wrong";
@@ -71,7 +71,7 @@ class MessengerMessageController extends APIController
         $data['files'] = $msFileData;
         $data['validations'] = app($this->requestValidationClass)->getDetailsByParams('id', $data['payload_value']);
         MessengerGroup::where('id', '=', $data['messenger_group_id'])->update(array('updated_at' => Carbon::now()));
-        Notifications::dispatch('message', $data->toArray());
+        Notifications::dispatch('message', $data);
       }else{
         $error = "Something went wrong";
       }
