65945abc1277f9b3b1dda2d8b8913306d74c8524
kennCK <kennettecanales@gmail.com>
2019-12-29 19:17:24 +0800

    update

--- src/http/MessengerMessageController.php
+++ src/http/MessengerMessageController.php
@@ -101,7 +101,7 @@ class MessengerMessageController extends APIController
           $payloadValue = $result[$i]['payload_value'];
           $this->response['data'][$i]['product'] = $this->getMessageByPayload($payload, $payloadValue);
           $this->response['data'][$i]['account'] = $this->retrieveAccountDetails($result[$i]['account_id']);
-          $this->response['data'][$i]['created_at_human'] = Carbon::createFromFormat('Y-m-d H:i:s', $result[$i]['created_at'])->copy()->tz('Asia/Manila')->format('F j, Y');
+          $this->response['data'][$i]['created_at_human'] = Carbon::createFromFormat('Y-m-d H:i:s', $result[$i]['created_at'])->copy()->tz('Asia/Manila')->format('F j, Y H:i A');
           $this->response['data'][$i]['files'] = app($this->msFileClass)->getByParams('messenger_message_id', $result[$i]['id']);
           $this->response['data'][$i]['validations'] = app($this->requestValidationClass)->getDetailsByParams('id', $result[$i]['payload_value']);
           $i++;
