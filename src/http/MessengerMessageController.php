838df7b0072dcac212579a4ec78b05567fefad02
kennCK <kennettecanales@gmail.com>
2020-01-03 18:49:38 +0800

    udpate

--- src/http/MessengerMessageController.php
+++ src/http/MessengerMessageController.php
@@ -65,6 +65,7 @@ class MessengerMessageController extends APIController
         $data['account'] = $this->retrieveAccountDetails($data['account_id']);
         $data['created_at_human'] =  Carbon::now()->copy()->tz('Asia/Manila')->format('F j, Y h:i A');
         $data['files'] = app($this->msFileClass)->getByParams('messenger_message_id', $this->response['data']);
+        $data['validations'] = null;
         MessengerGroup::where('id', '=', $data['messenger_group_id'])->update(array('updated_at' => Carbon::now()));
         Notifications::dispatch('message', $data);
       }else{
@@ -135,7 +136,10 @@ class MessengerMessageController extends APIController
           $this->response['data'][$i]['account'] = $this->retrieveAccountDetails($result[$i]['account_id']);
           $this->response['data'][$i]['created_at_human'] = Carbon::createFromFormat('Y-m-d H:i:s', $result[$i]['created_at'])->copy()->tz('Asia/Manila')->format('F j, Y h:i A');
           $this->response['data'][$i]['files'] = app($this->msFileClass)->getByParams('messenger_message_id', $result[$i]['id']);
-          $this->response['data'][$i]['validations'] = app($this->requestValidationClass)->getDetailsByParams('id', $result[$i]['payload_value']);
+          $this->response['data'][$i]['validations'] = null;
+          if($result[$i]['payload_value'] != null && intval($result[$i]['payload_value']) > 0){
+            $this->response['data'][$i]['validations'] = app($this->requestValidationClass)->getDetailsByParams('id', $result[$i]['payload_value']);
+          }
           $i++;
         }
       }
