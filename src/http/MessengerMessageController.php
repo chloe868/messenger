2a3b2a768b6219d724431e5da0562a4dbc026997
kennck <kennettecanales@gmail.com>
2021-02-23 08:30:38 +0800

    update

--- src/http/MessengerMessageController.php
+++ src/http/MessengerMessageController.php
@@ -34,6 +34,7 @@ class MessengerMessageController extends APIController
         $data['account'] = $this->retrieveAccountDetails($data['account_id']);
         $data['created_at_human'] =  Carbon::now()->copy()->tz($this->response['timezone'])->format('F j, Y h:i A');
         $data['custom_id'] = $data['messenger_group_id'];
+        $data['topic'] = $data['message'];
         MessengerGroup::where('id', '=', $data['messenger_group_id'])->update(array('updated_at' => Carbon::now()));
         $data['title'] = 'New Message';
         Notifications::dispatch('message', $data);
